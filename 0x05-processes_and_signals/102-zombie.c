329feca393c4b71a105bf4c60f5cc973b2fc69fb
Gekko Wrld <gekkowrld@gmail.com>
2024-02-23 22:10:51 +0300

    chore: Create zombie processes
    Create a program that creates zombie children.
    Only five are created and it enters a infinite sleep loop.
    The zombies created can be examined by:
    
    ```bash
    ps aux | grep -e 'Z+.*<defunct>'
    ```
    
    *Its cross platform (fingers crossed)
    
    Signed-off-by: Gekko Wrld <gekkowrld@gmail.com>

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void);
+
+/**
+ * main - Create zombie children and abandon them
+ *
+ * After creation of a zombie process, enter an infinite loop
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int zombies_num = 5;
+
+	while (zombies_num > 0)
+	{
+		if (fork() == 0)
+		{
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
+		} else
+		{
+			zombies_num--;
+		}
+	}
+
+	infinite_while();
+}
+
+/**
+ * infinite_while - Enter a (?) loop
+ *
+ * Return: 0
+*/
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
